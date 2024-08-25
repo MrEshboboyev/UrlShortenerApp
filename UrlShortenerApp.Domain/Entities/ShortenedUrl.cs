0ffcab4cd79674ef3c49b6baf97d6e8f11c18916
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-25 14:35:15 +0500

    Domain : Create Entities/ShortenedUrl.cs : Id, LongUrl, ShortUrl, CreatedAt, ExpirationDate, Description, HitCount, CustomAlias;

---
+++ UrlShortenerApp.Domain/Entities/ShortenedUrl.cs
@@ -0,0 +1,14 @@
+﻿namespace UrlShortenerApp.Domain.Entities
+{
+    public class ShortenedUrl
+    {
+        public int Id { get; set; }
+        public string LongUrl { get; set; } 
+        public string ShortUrl { get; set; } 
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public string Description { get; set; }
+        public int HitCount { get; set; } = 0;
+        public string CustomAlias { get; set; }
+    }
+}
