ad6a7b56415f05c7844aa9d0aaa02db6edc1f173
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-25 15:42:26 +0500

    Application : Project Reference : Domain; Common/Interfaces : create IShortenedUrlRepository.cs -> GetByShortUrl(), Create();

---
+++ UrlShortenerApp.Application/Common/Interfaces/IShortenedUrlRepository.cs
@@ -0,0 +1,10 @@
+﻿using UrlShortenerApp.Domain.Entities;
+
+namespace UrlShortenerApp.Application.Common.Interfaces
+{
+    public interface IShortenedUrlRepository
+    {
+        Task<ShortenedUrl> GetByShortUrl(string shortUrl);
+        Task Create(ShortenedUrl shortenedUrl);
+    }
+}
